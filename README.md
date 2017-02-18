e8d02f767479652a076180ebbc05cc9acfab858c
root <logicmoo@gmail.com>
2017-02-18 11:19:53 -0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # instant_prolog_docs
 Magically document prolog source files based on predicate and variable naming conventions
+
+
+```prolog
+
+ :- pack_install('https://github.com/TeamSPoon/instant_prolog_docs.git').
+
+```
+
+
+```prolog
+
+:- use_module(library(instant_prolog_docs)).
+
+?- autodoc_file('some_file.pl').
+
+```
+
+I haven't ran this for a very very long time so carefull.
+
+TODO:  
+  Make the Anglification system read a : instant_prolog_docs.config  (instead of being in the module itself)
---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(instant_prolog_docs).
+version('0.0.1').
+title('Magically document prolog source files based on predicate and variable naming conventions').
+keywords([tools,pldoc,documentation]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/TeamSPoon/instant_prolog_docs' ).
+download( 'https://github.com/TeamSPoon/instant_prolog_docs/release/*.zip' ).
+
