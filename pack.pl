1128a463ed6e04796c336dd06801a97e1d0cd696
root <logicmoo@gmail.com>
2017-02-26 10:48:31 -0800

    17-02-26-10:48:31

--- pack.pl
+++ pack.pl
@@ -3,8 +3,8 @@ version('0.0.8').
 title('Magically document prolog source files based on predicate and variable naming conventions').
 keywords([tools,pldoc,documentation]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
-packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
-maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
+maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home( 'https://github.com/TeamSPoon/instant_prolog_docs' ).
 download( 'https://github.com/TeamSPoon/instant_prolog_docs/release/*.zip' ).
 requires(must_trace).
