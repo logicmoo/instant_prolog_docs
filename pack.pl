131888a7337e48662b50f52ee4b7939e0a49fcfe
Douglas R. Miles <logicmoo@gmail.com>
2018-12-18 16:59:15 +0000

    1.2.111

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(instant_prolog_docs).
-version('1.1.118').
+version('1.2.111').
 title('Magically document prolog source files based on predicate and variable naming conventions').
 keywords([tools,pldoc,documentation]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
@@ -7,6 +7,6 @@ packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home( 'https://github.com/TeamSPoon/instant_prolog_docs' ).
 download( 'https://github.com/TeamSPoon/instant_prolog_docs/release/*.zip' ).
-requires(must_trace).
+requires(logicmoo_utils).
 autoload(false).
 
